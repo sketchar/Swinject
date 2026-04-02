fb45d294e6667b21667e4de48cb6a11dce6b23b7
Aleksandr Novikov <novikov@sketchar.io>
2026-04-02 21:36:36 +0700

    debug: add registration/resolution logging

--- Sources/Container.swift
+++ Sources/Container.swift
@@ -178,6 +178,7 @@ public final class Container {
     ) -> ServiceEntry<Service> {
         syncIfEnabled {
             let key = ServiceKey(serviceType: Service.self, argumentsType: Arguments.self, name: name, option: option)
+            NSLog("🟢 Swinject REG: service=\(Service.self) args=\(Arguments.self) name=\(name ?? "nil")")
             let entry = ServiceEntry(
                 serviceType: serviceType,
                 argumentsType: Arguments.self,
@@ -316,6 +317,9 @@ extension Container: _Resolver {
             var resolvedInstance: Service?
             let key = ServiceKey(serviceType: Service.self, argumentsType: Arguments.self, name: name, option: option)
 
+            if getEntry(for: key) == nil {
+                NSLog("🔴 Swinject MISS: service=\(Service.self) args=\(Arguments.self) name=\(name ?? "nil")")
+            }
 
             if key == Self.graphIdentifierKey {
                 return currentObjectGraph as? Service
